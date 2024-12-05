b91154847af309f0493e76f64f2510042d662d71
Duong Ngoc Phuong Anh <dnpanh.ityu@gmail.com>
2024-12-06 00:52:52 +0700

    1

--- IBkTuning.java
+++ IBkTuning.java
@@ -2,30 +2,30 @@ import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.lazy.IBk;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import java.util.Random;
 
-public class IBkClassifier {
+public class IBkTuning {
     public static void main(String[] args) throws Exception {
         DataSource trainSource = new DataSource("C:\\Users\\tonga\\IdeaProjects\\DataMining-Project\\Data\\training_data.arff");
         Instances trainDataset = trainSource.getDataSet();
-        DataSource testSource = new DataSource("C:\\Users\\tonga\\IdeaProjects\\DataMining-Project\\Data\\testing_data.arff");
-        Instances testDataset = testSource.getDataSet();
 
         trainDataset.setClassIndex(trainDataset.numAttributes() - 1);
-        testDataset.setClassIndex(testDataset.numAttributes() - 1);
+
+        String[] options = new String[6];
+        options[0] = "-K"; options[1] = "3";
+        options[2] = "-W"; options[3] = "0";
+        options[4] = "-A"; options[5] = "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.ManhattanDistance -R first-last\"";
 
         IBk ibk = new IBk();
+        ibk.setOptions(options);
         ibk.buildClassifier(trainDataset);
 
-        System.out.println("IBk params" + String.join(" ", ibk.getOptions()));
-
         Evaluation eval = new Evaluation(trainDataset);
-        eval.evaluateModel(ibk, testDataset);
+        eval.crossValidateModel(ibk, trainDataset, 10, new Random(42));
 
-        // Print the confusion matrix
-        System.out.println("Confusion Matrix:\n" + eval.toMatrixString());
-
-        // Print additional evaluation metrics
+        System.out.println("IBk Hyperparameters: " + String.join(" ", ibk.getOptions()));
         System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+        System.out.println("Confusion Matrix:\n" + eval.toMatrixString());
         System.out.println("Precision = " + eval.precision(1));
         System.out.println("Recall = " + eval.recall(1));
         System.out.println("F-Measure = " + eval.fMeasure(1));
